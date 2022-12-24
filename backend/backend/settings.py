3e6f32de7312afdf0bc52b846eaf0ef3816f084b
BADIUDragos <dragos.badiu.a@hotmail.com>
2022-12-24 18:32:12 -0500

    fixed media root

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -129,7 +129,7 @@ MEDIA_URL = '/images/'
 
 STATICFILES_DIRS = [os.path.join(BASE_DIR, 'backend/static')]
 
-MEDIA_ROOT = 'static/images'
+MEDIA_ROOT = 'backend/static/images'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
