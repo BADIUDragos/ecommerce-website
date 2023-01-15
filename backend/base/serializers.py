e68e4a090e5006a8113b46325210d55b1f3eaa65
BADIUDragos <dragos.badiu.a@hotmail.com>
2023-01-15 00:06:45 -0500

    no message

--- backend/base/serializers.py
+++ backend/base/serializers.py
@@ -1,6 +1,45 @@
 from rest_framework import serializers
+from rest_framework_simplejwt.tokens import RefreshToken
 from django.contrib.auth.models import User
 from .models import *
+
+
+class UserSerializer(serializers.ModelSerializer):
+
+    name = serializers.SerializerMethodField(read_only=True)
+    _id = serializers.SerializerMethodField(read_only=True)
+    isAdmin = serializers.SerializerMethodField(read_only=True)
+
+    class Meta:
+        model = User
+        fields = ["_id", 'username', 'email', 'name', 'isAdmin']
+
+    def get_name(self, obj):
+        name = obj.first_name
+        if name == '':
+            name = obj.email
+
+        return name
+
+    def get__id(self, obj):
+        _id = obj._id
+        return _id
+
+    def get_isAdmin(self, obj):
+        isAdmin = obj.is_staff
+        return isAdmin
+
+class UserSerializerWithToken(UserSerializer):
+
+    token = serializers.SerializerMethodField(read_only=True)
+
+    class Meta:
+        model = User
+        fields = ["_id", 'username', 'email', 'name', 'isAdmin', 'token']
+
+    def get_token(self, obj):
+        token = RefreshToken.for_user(obj)
+        return str(token)
 
 
 class ProductSerializer(serializers.ModelSerializer):
