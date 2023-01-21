2098d216e562150252dd865e311f5539a17b4160
BADIUDragos <dragos.badiu.a@hotmail.com>
2023-01-21 12:45:27 -0500

    updated from createStore to configureStore

--- frontend/src/store.js
+++ frontend/src/store.js
@@ -1,17 +1,9 @@
-import { createStore, combineReducers, applyMiddleware } from 'redux'
+
 import thunk from 'redux-thunk'
-import { composeWithDevTools } from 'redux-devtools-extension'
-import { productListReducer, productDetailsReducer,  } from './reducers/productReducers'
+import { configureStore } from '@reduxjs/toolkit'
+import { productListReducer, productDetailsReducer } from './reducers/productReducers'
 import { cartReducer } from './reducers/cartReducers'
 import { userLoginReducer, userRegisterReducer } from './reducers/userReducers'
-
-const reducer = combineReducers({
-    productList: productListReducer,
-    productDetails: productDetailsReducer,
-    cart: cartReducer,
-    userLogin: userLoginReducer,
-    userRegister: userRegisterReducer
-})
 
 const cartItemsFromStorage = localStorage.getItem('cartItems') ?
     JSON.parse(localStorage.getItem('cartItems')) : []
@@ -19,12 +11,20 @@ const cartItemsFromStorage = localStorage.getItem('cartItems') ?
 const userInfoFromStorage = localStorage.getItem('userInfo') ?
     JSON.parse(localStorage.getItem('userInfo')) : null
 
-const initialState = {
-    cart: {cartItems: cartItemsFromStorage},
-    userLogin: {userInfo: userInfoFromStorage}
-}
-const middleware = [thunk]
-const store = createStore(reducer, initialState, 
-    composeWithDevTools(applyMiddleware(...middleware)))
+const store = configureStore({
+    reducer: {
+        productList: productListReducer,
+        productDetails: productDetailsReducer,
+        cart: cartReducer,
+        userLogin: userLoginReducer,
+        userRegister: userRegisterReducer
+    },
+    middleware: [thunk],
+    preloadedState: {
+        cart: {cartItems: cartItemsFromStorage},
+        userLogin: {userInfo: userInfoFromStorage}
+    },
+    devTools: true
+})
 
 export default store
