3f48df86722ea3256d8634d9f72a05fec867b694
BADIUDragos <dragos.badiu.a@hotmail.com>
2023-01-21 00:48:31 -0500

    login logout working/ updating state

--- frontend/src/actions/userActions.js
+++ frontend/src/actions/userActions.js
@@ -1,44 +1,49 @@
-import axios from 'axios'
-import { 
-    USER_LOGIN_REQUEST,
-    USER_LOGIN_SUCCESS,
-    USER_LOGIN_FAIL,
-    USER_LOGOUT } 
-from '../constants/userConstants'
-
+import axios from "axios";
+import {
+  USER_LOGIN_REQUEST,
+  USER_LOGIN_SUCCESS,
+  USER_LOGIN_FAIL,
+  USER_LOGOUT,
+} from "../constants/userConstants";
 
 export const login = (email, password) => async (dispatchEvent) => {
-    try{
-        dispatchEvent({
-            type: USER_LOGIN_REQUEST
-        }) 
+  try {
+    dispatchEvent({
+      type: USER_LOGIN_REQUEST,
+    });
 
-        const config = {
-            headers:{
-                'Content-type': 'application/json'
-            }
-        }
+    const config = {
+      headers: {
+        "Content-type": "application/json",
+      },
+    };
 
-        const {data} = await axios.post(
-            '/api/users/login/',
-            {'username':email, 'password': password},
-            config
-            )
-        
-        dispatchEvent({
-            type: USER_LOGIN_SUCCESS,
-            payload: data
-        })
+    const { data } = await axios.post(
+      "/api/users/login/",
+      { username: email, password: password },
+      config
+    );
 
-        localStorage.setItem('userInfo', JSON.stringify(data))
+    dispatchEvent({
+      type: USER_LOGIN_SUCCESS,
+      payload: data,
+    });
 
-        
-    }catch(error){
-        dispatchEvent({
-            type: USER_LOGIN_FAIL,
-            payload:error.response && error.response.data.detail
-                ? error.response.data.detail
-                : error.message,
-        })
-    }
-}
+    localStorage.setItem("userInfo", JSON.stringify(data));
+  } catch (error) {
+    dispatchEvent({
+      type: USER_LOGIN_FAIL,
+      payload:
+        error.response && error.response.data.detail
+          ? error.response.data.detail
+          : error.message,
+    });
+  }
+};
+
+export const logout = () => (dispatchEvent) => {
+  localStorage.removeItem("userInfo");
+  dispatchEvent({
+    type: USER_LOGOUT,
+  });
+};
